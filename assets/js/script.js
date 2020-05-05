6c645e86f0ac8ddb7fcb877588e1de3e770a5f18
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-05 11:56:50 +0200

    Exercice corriger

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function newPictures() {
+  document.getElementById('imgPont').src = 'assets/img/pont2.jpg';
+}
+function oldPicture() {
+  document.getElementById('imgPont').src = 'assets/img/pont.jpg';
+}
